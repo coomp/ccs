2b80aa0e16bbbac3a779ad6f473633282ac7ff5a
ohrealfeng <249672168@qq.com>
2021-06-22 00:44:26 +0800

    commit net pool

--- def/NetDef.go
+++ def/NetDef.go
@@ -6,7 +6,7 @@ const (
 	SendAndRecvKeepalive   = 2 // 使用tcp连接池
 	SendOnlyKeepalive      = 3 // tcp连接池只发不收
 	SendOnly               = 4 // 只发不收
-	SendAndRecvIgnoreError = 5 // web长轮询技术，没有消息返回超时是正常的，应该忽略不上报l5
+	SendAndRecvIgnoreError = 5 // web长轮询技术
 	SendAndRecvStream      = 6 // tcp stream transport
 	SendOnlyStream         = 7 // tcp流一直发
 )
